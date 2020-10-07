56f489590fe41b94732e6735755518e098e82991
Justin Zollars <fogonthedowns@gmail.com>
2020-10-06 18:12:08 -0700

    update readme

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+/**
+ * // This is the robot's control interface.
+ * // You should not implement it, or speculate about its implementation
+ * type Robot struct {
+ * }
+ *
+ * // Returns true if the cell in front is open and robot moves into the cell.
+ * // Returns false if the cell in front is blocked and robot stays in the current cell.
+ * func (robot *Robot) Move() bool {}
+ *
+ * // Robot will stay in the same cell after calling TurnLeft/TurnRight.
+ * // Each turn will be 90 degrees.
+ * func (robot *Robot) TurnLeft() {}
+ * func (robot *Robot) TurnRight() {}
+ *
+ * // Clean the current cell.
+ * func (robot *Robot) Clean() {}
+ */
+
+// Time and Space complexity O(N-M)
+//   N is the number of cells
+//   M is the number of obsticles
+
 type Set map[[2]int]bool
 
 func cleanRoom(robot *Robot) {
